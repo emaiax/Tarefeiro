1e68bc7bbc4d95b6de5a27aaac72f1d06842d9bb
abrandao <andersonbrandao1982@bol.com.br>
2012-10-15 15:07:04 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tarefeiro::Application.config.secret_token = '0098bc0e0f99e98f38fe7356d4041e80ee6b2aa19acdc9bcb92a8c8e32527c340ff7d292b5627fa1b9822327ecae4f0cf2a8773d5c3f43846a5dc965a81f2cb5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tarefeiro::Application.config.session_store :cookie_store, key: '_tarefeiro_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tarefeiro::Application.config.session_store :active_record_store
